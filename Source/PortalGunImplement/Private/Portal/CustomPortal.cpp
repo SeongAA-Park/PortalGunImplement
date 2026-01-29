c34d94986a72a67d5b6c50c2d041935dd85da22f
SeongAA-Park <seongah0201@gmail.com>
2026-01-29 18:16:48 +0900

    feat: Implement ClipPlane
    Applied ClipPlane to the scene capture component to eliminate foreground/background obstructions.
    
    Known Issue: Rendering still feels unnatural; suspected errors remain in the transformation calculation logic for the portal view.
    
    #9

--- Source/PortalGunImplement/Private/Portal/CustomPortal.cpp
+++ Source/PortalGunImplement/Private/Portal/CustomPortal.cpp
@@ -32,6 +32,9 @@ ACustomPortal::ACustomPortal()
 	// 3. 카메라는 루트에 붙입니다. (메쉬가 돌아가도 영향을 받지 않도록)
 	PortalCamera = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("PortalCamera"));
 	PortalCamera->SetupAttachment(RootComponent);
+	
+	//(추가) Clip 평면 설정을 위한 PortalCamera의 Capture Source 설정
+	PortalCamera->CaptureSource = ESceneCaptureSource::SCS_SceneColorHDR;
     
 	PortalCamera->bCaptureEveryFrame = false;
 	PortalCamera->bCaptureOnMovement = false;
@@ -62,6 +65,8 @@ void ACustomPortal::BeginPlay()
 			DynMat->SetTextureParameterValue(TEXT("PortalRendTex"),NewRT);
 		}
 	}
+	
+	
 }
 
 // Called every frame
@@ -93,15 +98,32 @@ void ACustomPortal::UpdatePortalView()
 	FQuat MirroredRotation = FlipQuat * CharNCurrentPotRelativeTransform.GetRotation();
 	FTransform MirroredTransform(MirroredRotation, MirroredLocation);
 	
-	// [Step 4] 반전된 시점을 상대방 포탈(B)의 월드 위치로 투영 (Mirrored * M_PortalB)
+	// 반전된 시점을 상대방 포탈(B)의 월드 위치로 투영 (Mirrored * M_PortalB)
 	FTransform FinalTransform = MirroredTransform * LinkedPortal->GetActorTransform();
 	UE_LOG(LogTemp, Warning, TEXT("%f, %f, %f"), FinalTransform.GetLocation().X, FinalTransform.GetLocation().Y, FinalTransform.GetLocation().Z);
 	
-	// [Step 5] 내 카메라를 최종 월드 위치로 이동 및 장면 캡처
+	// 내 카메라를 최종 월드 위치로 이동 및 장면 캡처
 	//LinkedPortal->PortalCamera->SetRelativeTransform(FinalTransform);
 	PortalCamera->SetWorldTransform(FinalTransform);
 	
-	// 5. 장면 캡처 실행 (수동 호출)
+	// 디버그 카메라 그리기 (빨간색, 0.1초 동안 유지되게 하여 매 프레임 갱신)
+	DrawDebugCamera(GetWorld(), FinalTransform.GetLocation(), FinalTransform.Rotator(), PortalCamera->FOVAngle, 1.0f, FColor::Red, false, 0.1f);
+	
+	//about Clip Plane 설정
+	{
+		//클립 평면 설정
+		PortalCamera->bEnableClipPlane = true;
+		
+		// 상대편 포탈의 위치를 기준으로 ClipPlane 설정 (해당 위치에서 바로 자르지 말고 조금 띄우는 것도 방법이여!)
+		PortalCamera->ClipPlaneBase = LinkedPortal->GetActorTransform().GetLocation()+LinkedPortal->GetActorForwardVector()*0.1;
+	
+		// ClipPlane 방향 설정(NormalVector 지정) - 상대편 포탈의 전방 벡터를 Normal로 지정
+		//PortalCamera->ClipPlaneNormal = LinkedPortal->GetActorUpVector();
+		//PortalCamera->ClipPlaneNormal = PortalCamera->GetForwardVector();
+		PortalCamera->ClipPlaneNormal = LinkedPortal->GetActorForwardVector();
+	}
+	
+	// 장면 캡처 실행 (수동 호출)
 	//LinkedPortal->PortalCamera->CaptureScene();
 	PortalCamera->CaptureScene();
 	
